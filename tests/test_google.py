991e39f9800415b09d4d438682b1a4ba36f7b3cf
Vamshi Krishna <vamshi.thota@beghouconsulting.com>
2026-02-09 18:49:32 +0530

    Initial Selenium CI setup

--- tests/test_google.py
+++ tests/test_google.py
@@ -10,5 +10,5 @@ def test_beghou_title():
     driver = webdriver.Chrome(options=options)
     driver.get("https://www.beghou.com")
 
-    assert "beghou" in driver.title
+    assert "Beghou" in driver.title
     driver.quit()
