d8762ab7f0ebc78c1bc8682aaf573e3c391bf4e7
Vamshi Krishna <vamshi.thota@beghouconsulting.com>
2026-02-13 12:19:07 +0530

    Initial Selenium CI setup

--- tests/test_google.py
+++ tests/test_google.py
@@ -7,7 +7,7 @@ def test_beghou_title():
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
 
-    driver = webdriver.Chrome(options=options)
+    driver = webdriver.Chrome(options=option3s)
     driver.get("https://www.beghou.com")
 
     assert "Beghou" in driver.title
